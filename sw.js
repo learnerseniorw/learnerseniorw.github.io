6d10b09756c673a1ff9f23e202ce264d8009855a
learnerseniorw <learnerseniorw@users.noreply.github.com>
2026-08-15 14:43:59 +0800

    Add Chinese translations (zh) to all 2917 sentences; show/hide toggle; version 2026-08-15.8

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * data.js uses stale-while-revalidate so updates come through when online.
  */
 const VERSION = "vocab-v2";
-const CACHE = VERSION + "-v2";
+const CACHE = VERSION + "-v3";
 
 const PRECACHE = [
   "./",
